2af5ec1d6d6f677930e23ac23fee7a09a14bd6df
Trapper <matthias.linder@tptlive.ee>
2018-05-10 08:35:51 +0300

    Initial Commit

---
+++ NotebookXML - 08.05.2018/NotebookXML - 08.05.2018/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotebookXML___08._05._2018
+{
+    public class Note
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ NotebookXML - 08.05.2018/NotebookXML - 08.05.2018/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NotebookXML___08._05._2018
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            #region Read Note
+            int SetID = 0;
+            var Notes = new List<Note>();
+            var serializer = new XmlSerializer(typeof(List<Note>));
+            using (var reader = XmlReader.Create("../../resources/note-doc.xml"))
+            {
+                Notes = (List<Note>)serializer.Deserialize(reader);
+            }
+            #endregion
+
+            foreach (var Note in Notes)
+            {
+                Console.WriteLine("Title : " + Note.Name);
+                Console.WriteLine("Content : " +Note.Content);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Please enter the Title of the Note");
+            var UserInputName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the contents of the Note");
+            var UserInputContent = Console.ReadLine();
+
+            var NewNote = new Note() { Name = UserInputName, Content = UserInputContent};
+            if (Notes.Count > 0)
+            {
+                var lastnote = Notes.Last();
+                SetID = lastnote.ID;
+                NewNote.ID = SetID + 1;
+            }
+            else
+            {
+                NewNote.ID = 0;
+            }
+            Notes.Add(NewNote);
+
+            #region Save Note
+            var Serializer = new XmlSerializer(Notes.GetType());
+            using (var writer = XmlWriter.Create("../../resources/note-doc.xml"))
+            {
+                serializer.Serialize(writer, Notes);
+            }
+            #endregion
+
+            Console.WriteLine("Thank you for using XMLNote");
+            Console.ReadLine();
+            
+        }
+    }
+}
